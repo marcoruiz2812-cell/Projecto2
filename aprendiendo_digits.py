19c1c6b47ca4df39946941332b0d85c6fcd19acc
Marco <marcoruiz2812@gmail.com>
2025-11-20 19:32:21 -0500

    Lectura de digitos
    Load digit data from CSV and process into arrays.

--- aprendiendo_digits.py
+++ aprendiendo_digits.py
@@ -6,6 +6,24 @@ misdatos = datasets.load_digits()
 X= misdatos.data
 y= misdatos.target
 
+data = np.loadtxt("numeros.csv", delimiter=",")
+
+digitos = []
+buffer = []
+for fila in data:
+    if np.all(fila == 0):
+        if len(buffer) > 0:
+            digitos.append(np.array(buffer))
+            buffer = []
+    else:
+        buffer.append(fila)
+if len(buffer) > 0:
+    digitos.append(np.array(buffer))
+
+#Flatten
+vectores = [mat.flatten() for mat in digitos]
+
+
 def distancia_eucli(a,b):
     suma= 0
     for i in range(len(a)):
